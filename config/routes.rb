1452656e1adef86197f8881f514d9bd3fe66f08a
yasumura-shunsuke <yasumura_shunsuke@kakaku.com>
2018-04-29 15:46:10 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'toppages#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'session#create'
+  delete 'logout', to: 'session#destroy'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
