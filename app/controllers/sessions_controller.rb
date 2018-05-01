af0f6bb9bfa26783470c62d48a8555a5fde8f9b2
yasumura-shunsuke <yasumura_shunsuke@kakaku.com>
2018-05-01 05:47:11 +0000

    post

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,15 @@ class SessionsController < ApplicationController
   def create
     email = params[:session][:email].downcase
     password = params[:session][:password]
-   if login(email, password)
+    if login(email, password)
       flash[:success] = 'ログインに成功しました。'
       redirect_to @user
     else
       flash.now[:danger] = 'ログインに失敗しました。'
       render 'new'
-   end
+    end
   end
+
 
   def destroy
     session[:user_id] = nil
@@ -22,11 +23,11 @@ class SessionsController < ApplicationController
   
   private
   
-  def login(email,password)
-    @user = User.find_by(email,email)
+  def login(email, password)
+    @user = User.find_by(email: email)
     if @user && @user.authenticate(password)
       # ログイン成功
-      session[user_id] = @user.id
+      session[:user_id] = @user.id
       return true
     else
       # ログイン失敗
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   root to: 'toppages#index'
   
   get 'login', to: 'sessions#new'
-  post 'login', to: 'session#create'
-  delete 'logout', to: 'session#destroy'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+  resources :microposts, only: [:create, :destroy]
 end
