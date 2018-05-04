61089c161e5ea75fd70be4d68b613d553fbe9a2c
yasumura-shunsuke <yasumura_shunsuke@kakaku.com>
2018-05-04 08:03:55 +0000

    follow, unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,16 @@ Rails.application.routes.draw do
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  delete 'login', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show, :new, :create] do
+    member do
+      get :followings
+      get :followers 
+    end
+  end
+  
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
